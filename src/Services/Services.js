a507c03432c855073db78932a212d994e16e35dc
adnan shaikh <adnn4u@gmail.com>
2023-09-04 18:33:38 +0530

    services

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "./Services.css"
 import pic2 from "../img/pic2.jpg"
+import pic3 from "../img/pic3.jpg"
+import pic4 from "../img/pic4.jpg"
+import pic5 from "../img/pic5.jpg"
+
+
 function Services() {
   return (
     <div className='Services'>
       <h2>OUR SERVICES</h2>
+      <div className="serv">
       <div className="card"  
       style={{backgroundImage: `url(${pic2})`,
     backgroundRepeat: 'no-repeat',
@@ -19,6 +25,51 @@ function Services() {
          <button>READ MORE</button>
       </div>
       </div>
+
+      <div className="card"  
+      style={{backgroundImage: `url(${pic3})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: "cover",
+    }}>
+      <div className="ser-txt">
+      <h2>3D Modeling</h2>
+      <p>3D Models can access the technical and 
+        financial viability of a design, construction
+         stability, function, performance...</p>
+         <button>READ MORE</button>
+      </div>
+      </div>
+      <div className="card"  
+      style={{backgroundImage: `url(${pic4})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: "cover",
+    }}>
+      <div className="ser-txt">
+      <h2>3D Modeling</h2>
+      <p>3D Models can access the technical and 
+        financial viability of a design, construction
+         stability, function, performance...</p>
+         <button>READ MORE</button>
+      </div>
+      </div>
+      <div className="card"  
+      style={{backgroundImage: `url(${pic5})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: "cover",
+    }}>
+      <div className="ser-txt">
+      <h2>3D Modeling</h2>
+      <p>3D Models can access the technical and 
+        financial viability of a design, construction
+         stability, function, performance...</p>
+         <button>READ MORE</button>
+      </div>
+      </div>
+      </div>
+      
     </div>
   )
 }
