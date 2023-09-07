8c15c0479c38813a92b4f6ea5a271f84a65e81ef
Adnan1O <adnn4u@gmail.com>
2023-09-07 15:24:20 +0530

    new navbar feature added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Home from './Home/Home';
+import Info from './Navbar/Info';
 import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Info/>
     <Navbar/>
     <Home/>
     </div>
---
+++ src/Navbar/Info.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import india from "../img/india.png"
+import aus from"../img/aus.png"
+import mail from "../img/mail.png"
+import { BsLinkedin } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
+import "./Info.css"
+function Info() {
+  return (
+    <div className='Info'>
+        <div className="info-area">
+      <div className="contact">
+        <div className="numb">
+            <img src={india} alt="" />
+            <span>+91 987654321</span>
+        </div>
+        <div className="numb">
+            <img src={aus} alt="" />
+            <span>+91 987654321</span>
+        </div>
+        <div className="numb">
+            <img src={mail} alt="" />
+            <span>contact@metalic.com</span>
+        </div>
+      </div>
+      <div className="socials">
+        <span>Blog</span>
+        <span>Socials:  <BsLinkedin color="#0072b1"/> <BsFacebook color="#1877F2"/></span>
+      
+      </div>
+    </div></div>
+  )
+}
+
+export default Info
