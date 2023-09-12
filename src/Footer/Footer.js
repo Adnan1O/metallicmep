1a55f019ff3ceadf5b12ef329de2cce5323c13f7
Adnan1O <adnn4u@gmail.com>
2023-09-12 15:26:48 +0530

    new section

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import "./Footer.css"
+import { BsLinkedin } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
+import { BsYoutube } from 'react-icons/bs';
 function Footer() {
   return (
     <div className='Footer'>
-      hh
+      <div className="first">
+        <h2>Archdraw Outsourcing</h2>
+        <p>We offer the High Quality CAD
+           drafting, BIM modeling & Architectural Visualization
+            services worldwide.</p>
+            <div className="social">
+            <BsLinkedin color="#0072b1"/>
+         <BsFacebook color="#1877F2"/>
+         <BsYoutube color="#CD201F"/>
+            </div>
+      </div>
+      <div className="second">
+        <h2>Quick Links</h2>
+        <ul>
+        <li>Our Services</li>
+        <li>About Us</li>
+        <li>Project</li> 
+        <li>Career</li>
+        <li className='contact'>Contact</li>
+      </ul>
+      </div>
     </div>
   )
 }
