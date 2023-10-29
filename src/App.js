de0e88fbf244610b2b1f8102f97a2c9ab5b16373
adnan shaikh <adnn4u@gmail.com>
2023-10-29 18:31:47 +0530

    main pages removed

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Footer from './Footer/Footer';
 import Home from './Home/Home';
 import Info from './Navbar/Info';
 import Navbar from './Navbar/Navbar';
+import Test from './Test/Test';
 
 function App() {
   return (
     <div className="App">
-      <div className="info">
+      {/* <div className="info">
       <Info/>
       </div>
     <Navbar/>
     <Home/>
-    <Footer/>
+    <Footer/> */}
+    <Test/>
     </div>
   );
 }
