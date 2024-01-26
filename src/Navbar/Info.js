44b4fae5817f14250de84254e07853dd0fc488de
adnan shaikh <adnn4u@gmail.com>
2024-01-26 18:16:08 +0530

    styles added

--- src/Navbar/Info.js
+++ src/Navbar/Info.js
@@ -1,44 +1,44 @@
-// import React from 'react'
-// import india from "../img/india.png"
-// import aus from"../img/aus.png"
-// import mail from "../img/mail.png"
-// <<<<<<< HEAD
-// // import { BsLinkedin } from 'react-icons/bs';
-// // import { BsFacebook } from 'react-icons/bs';
-// =======
+import React from 'react'
+import india from "../img/india.png"
+import aus from"../img/aus.png"
+import mail from "../img/mail.png"
+<<<<<<< HEAD
 // import { BsLinkedin } from 'react-icons/bs';
 // import { BsFacebook } from 'react-icons/bs';
-// import { BsYoutube } from 'react-icons/bs';
-// >>>>>>> 1a55f019ff3ceadf5b12ef329de2cce5323c13f7
-// import "./Info.css"
-// function Info() {
-//   return (
-//     <div className='Info'>
-//         <div className="info-area">
-//       <div className="contact">
-//         <div className="numb">
-//             <img src={india} alt="" />
-//             <span>+91 987654321</span>
-//         </div>
-//         <div className="numb">
-//             <img src={aus} alt="" />
-//             <span>+1 987654321</span>
-//         </div>
-//         <div className="numb">
-//             <img src={mail} alt="" />
-//             <span>contact@metalic.com</span>
-//         </div>
-//       </div>
-//       <div className="socials">
-//         <span>Blog</span>
-//         <span>Socials:  <BsLinkedin color="#0072b1"/>
-//          <BsFacebook color="#1877F2"/>
-//          <BsYoutube color="#CD201F"/>
-//           </span>
+=======
+import { BsLinkedin } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
+import { BsYoutube } from 'react-icons/bs';
+>>>>>>> 1a55f019ff3ceadf5b12ef329de2cce5323c13f7
+import "./Info.css"
+function Info() {
+  return (
+    <div className='Info'>
+        <div className="info-area">
+      <div className="contact">
+        <div className="numb">
+            <img src={india} alt="" />
+            <span>+91 987654321</span>
+        </div>
+        <div className="numb">
+            <img src={aus} alt="" />
+            <span>+1 987654321</span>
+        </div>
+        <div className="numb">
+            <img src={mail} alt="" />
+            <span>contact@metalic.com</span>
+        </div>
+      </div>
+      <div className="socials">
+        <span>Blog</span>
+        <span>Socials:  <BsLinkedin color="#0072b1"/>
+         <BsFacebook color="#1877F2"/>
+         <BsYoutube color="#CD201F"/>
+          </span>
       
-//       </div>
-//     </div></div>
-//   )
-// }
+      </div>
+    </div></div>
+  )
+}
 
-// export default Info
+export default Info
