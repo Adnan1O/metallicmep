4c3ff8e793e496b6e5c477c0ffa601910a34eef9
adnan shaikh <adnn4u@gmail.com>
2024-02-11 23:56:13 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 
-// import './App.css';
-// import Footer from './Footer/Footer';
-// import Home from './Home/Home';
-// import Info from './Navbar/Info';
-// import Navbar from './Navbar/Navbar';
-// import Test from './Test/Test';
+import './App.css';
+import Footer from './Footer/Footer';
+import Home from './Home/Home';
+import Info from './Navbar/Info';
+import Navbar from './Navbar/Navbar';
+import Test from './Test/Test';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <div className="info">
-//       <Info/>
-//       </div>
-//       <Navbar/>
-//       <Home/>
-//       <Footer/>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <div className="info">
+      <Info/>
+      </div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
